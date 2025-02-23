d1cd503e7e65dd8c597d9aff1ae8ef54e260ac2d
Victor Hugo <victor.hugo.antunes.n@gmail.com>
2025-02-23 17:09:06 -0300

    feat: adiciona endpoint de registro de usuário

---
+++ src/Backend/OngsPetAPI/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OngsPet.Communication.Requests;
+using OngsPet.Communication.Responses;
+
+namespace OngsPet.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterUserDTO), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserDTO request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/Shared/OngsPet.Communication/Responses/ResponseRegisterUserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngsPet.Communication.Responses
+{
+    public class ResponseRegisterUserDTO
+    {
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
